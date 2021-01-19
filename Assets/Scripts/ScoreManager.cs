a265578ea3b3553e7147952ad52b750e8468a20f
Patryk Paluch <patrykas1000@gmail.com>
2021-01-19 13:43:06 +0100

    Add ScoreManager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	public delegate void ScoreChangedEvent(int newScore);
+	
+	public static ScoreManager Instance { get; private set;  }
+
+	public int Score {
+		get => score;
+		set {
+			if (score != value) {
+				score = value;
+				OnScoreChange?.Invoke(score);
+			}
+		}
+	}
+	public ScoreChangedEvent OnScoreChange;
+
+
+	private int score;
+	private void Awake() {
+		if (Instance != null) {
+			Debug.LogError("Multiple instances of ScoreManager", this);
+			Destroy(this);
+			return;
+		}
+
+		Instance = this;
+		score = 0;
+	}
+	
+}
